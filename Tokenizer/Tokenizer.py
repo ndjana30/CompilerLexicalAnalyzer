a43ea393260ce6204499843c82c00e1230cfd74e
ndjana30 <davyndjana@gmail.com>
2023-01-09 22:44:00 -0800

    new commit

--- Tokenizer/Tokenizer.py
+++ Tokenizer/Tokenizer.py
@@ -1,12 +1,20 @@
 from normalizer import Normalizer
+import spacy
 
 class Tokenizer():
     def __init__(self)->None:
-        self._n =Normalizer.verify_sentence(self)
+        self._n =Normalizer.verify_sentence(self) #The text to split
     def tokenizer(self)->list:
-        self._tokens = self._n.split()
+        self._tokens = []
+        self._nlp = spacy.load('en_core_web_sm')
+        self._doc= self._nlp(self._n)
+        for tokens in self._doc:
+            self._tokens.append(str(tokens))
+        return (self._tokens)
+        # return (string)
+        # return str(self._tokens)
         
-        # print(self._tokens)
-        return self._tokens
+        # self._tokens = self._doc.split()
+        # return self._tokens
 t=Tokenizer()
 t.tokenizer()
